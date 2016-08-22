de5c616be98d237aba6212ccc82676708f05750d
achieven <achievendar@gmail.com>
2016-08-22 17:50:13 +0300

    added link to emitter webpage and removed the tests for now

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 var express = require('express');
-
 var app = express();
+var Handlebars = require('handlebars');
+var fs = require('fs');
 
 app.get('/', function (req, res) {
-    res.send('hello jenkin');
+    var projects = [
+        {name: 'emitter', link:'http://emitter.achievendar.tk'}
+    ]
+    var html = Handlebars.compile(fs.readFileSync('./app.html', 'utf8'))({
+        projects:projects 
+    });
+    res.send(html);
 });
+app.use(express.static(__dirname + '/'));
 
 app.listen(process.env.PORT || 5000);
 
