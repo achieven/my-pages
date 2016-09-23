c6aca18c3fec34b14b92911cc47d23ef3877c296
achieven <achievendar@gmail.com>
2016-09-23 14:28:18 +0300

    added github code for main and sub projects, added some decoration

--- app.js
+++ app.js
@@ -5,17 +5,22 @@ var fs = require('fs');
 
 app.get('/', function (req, res) {
     var projects = [
-        {name: 'emitter', link:'http://emitterachi.achievendar.tk'},
-        {name: 'backend', link:'http://backend.achievendar.tk'},
-        {name: 'Simple Rest Api', link:'http://simplerestapi.achievendar.tk'}
+        {name: 'emitter', link:'http://emitterachi.achievendar.tk', githubLink: 'https://github.com/achieven/emitter'},
+        {name: 'backend', link:'http://backend.achievendar.tk', githubLink: 'https://github.com/achieven/backend'},
+        {name: 'Simple Rest Api', link:'http://simplerestapi.achievendar.tk', githubLink: 'https://github.com/achieven/simplerestapi'}
     ]
+    var mainProjectGithubLink = 'https://github.com/achieven/my-pages'
     var html = Handlebars.compile(fs.readFileSync('./app.html', 'utf8'))({
-        projects:projects 
+        projects:projects,
+        mainProjectGithubLink: mainProjectGithubLink
     });
     res.send(html);
 });
 app.use(express.static(__dirname + '/'));
 
-app.listen(process.env.PORT || 5000);
+const port = process.env.PORT || 5000
+app.listen(port, function(){
+    console.log('listening on port',  port)
+});
 
 module.exports = app;
